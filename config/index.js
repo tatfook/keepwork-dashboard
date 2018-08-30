5595a47e36c2e57f94a1dc4bcd3cc444148a8cd9
Leopold Chen <placeintime@live.cn>
2018-08-30 15:01:16 +0800

    use cheap-module-eval-source-map instead

--- config/index.js
+++ config/index.js
@@ -32,7 +32,7 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-source-map',
+    devtool: 'cheap-module-eval-source-map',
 
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
