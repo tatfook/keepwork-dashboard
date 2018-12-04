8c2f4a767da19207e204b937ef48394e3b8375b9
onedou <onedou@126.com>
2018-12-04 10:51:18 +0800

    update userinfo api and resources

--- src/resources/userinfo.js
+++ src/resources/userinfo.js
@@ -1,44 +1,100 @@
-import { resourceCRUD } from '@/api/keepwork'
+import { userinfoCRUD } from '@/api/userinfo'
 import BaseResource from './base'
 
-const crudAPI = resourceCRUD('users')
+const crudAPI = userinfoCRUD()
+
+const sexMap = [
+  {
+    key: 'F',
+    value: '女'
+  },
+  {
+    key: 'M',
+    value: '男'
+  },
+  {
+    key: 'N',
+    value: '未知'
+  }
+]
+
+const levelMap = [
+  {
+    key: 'VIP',
+    value: 'VIP'
+  },
+  {
+    key: 'ordinary',
+    value: '普通'
+  }
+]
+
+const statusMap = [
+  {
+    key: true,
+    value: '正常'
+  },
+  {
+    key: false,
+    value: '封停'
+  }
+]
 
 export default class Userinfo extends BaseResource {
   static attributes() {
     return [
       {
         name: 'username',
-        title: true
+        title: true,
+        edit: false
       },
       {
         name: 'createdAt',
-        title: true,
         type: 'Date',
-        edit: false
+        edit: false,
+        search: false
       },
       {
         name: 'cellphone',
         type: 'String',
         required: true,
         component: 'text',
-        title: true,
         edit: true
       },
       {
         name: 'level',
         type: 'String',
         required: true,
-        component: 'text',
-        title: true,
-        edit: true
+        component: 'select',
+        edit: false,
+        options: levelMap,
+        filter: (key) => {
+          for (const item of levelMap) {
+            if (item.key === key) {
+              return item.value
+            }
+          }
+
+          return key
+        },
+        search: false
       },
       {
         name: 'status',
         type: 'String',
         required: true,
         component: 'text',
-        title: true,
-        edit: true
+        edit: false,
+        filter: (key) => {
+          for (const item of statusMap) {
+            if (item.key === key) {
+              return item.value
+            }
+          }
+
+          return key
+        },
+        search: false
       },
       {
         name: 'balance',
@@ -46,31 +102,41 @@ export default class Userinfo extends BaseResource {
         required: true,
         component: 'text',
         title: true,
-        edit: true
+        edit: false,
+        search: false
       },
       {
         name: 'coin',
         type: 'String',
         required: true,
         component: 'text',
-        title: true,
-        edit: true
+        edit: false,
+        search: false
       },
       {
         name: 'bean',
         type: 'String',
         required: true,
         component: 'text',
-        title: true,
-        edit: true
+        edit: false,
+        search: false
       },
       {
         name: 'sex',
         type: 'String',
         required: true,
-        component: 'text',
-        title: true,
-        edit: true
+        component: 'select',
+        edit: true,
+        options: sexMap,
+        filter: (key) => {
+          for (const item of sexMap) {
+            if (item.key === key) {
+              return item.value
+            }
+          }
+
+          return sexMap.N.value
+        }
       }
     ]
   }
@@ -81,7 +147,7 @@ export default class Userinfo extends BaseResource {
 
   static actions() {
     return {
-      disabled: []
+      disabled: ['create']
     }
   }
 }
