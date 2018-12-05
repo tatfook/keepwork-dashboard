e38da2598c57f732d7774d325d91e6a27353a3a6
qinghua.chen <placeintime@live.cn>
2018-12-05 17:55:32 +0800

    fix i18n for production

--- src/resources/base.js
+++ src/resources/base.js
@@ -3,6 +3,7 @@ import _ from 'lodash'
 import {
   i18n
 } from '../i18n'
+import store from '@/store'
 
 export default class BaseResource {
   constructor(row) {
@@ -139,7 +140,9 @@ export default class BaseResource {
   }
 
   static i18n(col) {
-    return i18n.t(['resources', this.name, 'attr', col].join('.'))
+    // FIXME
+    // this.name is not working on production
+    return i18n.t(['resources', store.getters.resourceName, 'attr', col].join('.'))
   }
 
   static i18nBase(attr) {
