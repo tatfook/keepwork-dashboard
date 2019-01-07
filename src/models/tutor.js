a2f7efb5022e1fe28fe344a9c0b77d0600678966
onedou <onedou@126.com>
2019-01-07 11:25:57 +0800

    1. update tutor 2. add createable filter

--- src/models/tutor.js
+++ src/models/tutor.js
@@ -1,17 +1,17 @@
 import { resourceCRUD as lessonCRUD } from '../api/lesson'
-import _ from 'lodash'
+// import _ from 'lodash'
 
 const tutorCURD = lessonCRUD('tutors')
 
 export default function tutorModel() {
   return {
     async list(params) {
-      console.log(_)
-      console.log(tutorCURD)
+      // console.log(_)
+      // console.log(tutorCURD)
 
       const tutorList = tutorCURD.list(params)
 
-      console.log(tutorList)
+      // console.log(tutorList)
 
       return tutorList
     },
--- src/resources/tutor.js
+++ src/resources/tutor.js
@@ -8,16 +8,39 @@ export default class Tutor extends BaseResource {
         name: 'userId',
         show: true,
         edit: false,
+        create: false,
         search: true,
         associate: 'User',
         sort: false
       },
       {
+        name: 'cellphone',
+        show: true,
+        search: true,
+        insert: true,
+        edit: false,
+        component: 'input'
+      },
+      {
+        name: 'tutorId',
+        show: true,
+        search: false,
+        associate: 'User'
+      },
+      {
         name: 'endTime',
         type: 'Date',
         show: true,
-        edit: false,
-        search: false
+        search: false,
+        component: 'time'
+      },
+      {
+        name: 'status',
+        show: true
+      },
+      {
+        name: 'comment',
+        show: true
       }
     ]
   }
@@ -28,7 +51,7 @@ export default class Tutor extends BaseResource {
 
   static actions() {
     return {
-      disabled: []
+      disabled: ['show']
     }
   }
 }
