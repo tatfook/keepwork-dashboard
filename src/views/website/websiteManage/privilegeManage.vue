a2337810dc1ea4ee9894ed1d24f0ba7aa0bae0e9
Jan <1720370638@qq.com>
2019-01-18 14:58:08 +0800

    1.update websiteManage 2.update i18n 3.add privilegeManage component

---
+++ src/views/website/websiteManage/privilegeManage.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-table :data="list" border style="width: 100%">
+    <el-table-column :label="$t('dialog.label.groupsName')" fixed>
+      <template slot-scope="scope">
+        <span>{{scope.row.groupName}}</span>
+      </template>
+    </el-table-column>
+    <el-table-column :label="$t('dialog.label.members')">
+      <template slot-scope="scope">
+        <span v-for="(name, index) in scope.row.userName" :key="index">{{name}},</span>
+      </template>
+    </el-table-column>
+    <el-table-column :label="$t('dialog.label.permission')">
+      <template slot-scope="scope">
+        <span>{{scope.row.level}}</span>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'PrivilegeManage',
+  props: {
+    list: Array
+  }
+}
+</script>
