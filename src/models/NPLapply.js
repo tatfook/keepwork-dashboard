497ff871c2a3d750d242089cf331cfd73e47866d
DDXY1230 <2502752288@qq.com>
2019-02-22 16:21:11 +0800

    fix bug

--- src/models/NPLapply.js
+++ src/models/NPLapply.js
@@ -1,4 +1,5 @@
 import createService from '@/utils/request'
+import _ from 'lodash'
 
 const request = createService()
 
@@ -7,6 +8,17 @@ export default {
     return request({
       method: 'get',
       url: 'games/members'
+    })
+  },
+  update(data) {
+    const params = {}
+    _.forEach(data, (value, key) => {
+      _.set(params, key, value)
+    })
+    return request({
+      method: 'put',
+      url: '/admins/userinfos/' + data.userinfosId,
+      data: params
     })
   }
 }
